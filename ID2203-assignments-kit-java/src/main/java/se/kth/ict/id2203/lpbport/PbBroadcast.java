78315df0f12977abb88321ab6a97f31b6ef1e8c2
fingolfin <fingolfin@Hithlum>
2013-02-11 14:59:17 +0100

    application 2

--- ID2203-assignments-kit-java/src/main/java/se/kth/ict/id2203/lpbport/PbBroadcast.java
+++ ID2203-assignments-kit-java/src/main/java/se/kth/ict/id2203/lpbport/PbBroadcast.java
@@ -5,11 +5,22 @@
 package se.kth.ict.id2203.lpbport;
 
 import se.sics.kompics.Event;
+import se.sics.kompics.address.Address;
 
 /**
  *
  * @author fingolfin
  */
 public class PbBroadcast extends Event{
+
+    private Address source;
+    public PbBroadcast(Address source) {
+        this.source = source;
+    }
+
+    public Address getSource() {
+        return source;
+    }
+    
     
 }

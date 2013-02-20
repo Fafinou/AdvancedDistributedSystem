f4ede3916ef81ec0c7adcf2b1df8f569866fa4d7
alex_geo <a.georgoudaki@gmail.com>
2013-02-20 17:19:34 +0100

    scenarios

--- ID2203-assignments-kit-java/src/main/java/se/kth/ict/id2203/Assignment3abExecutor.java
+++ ID2203-assignments-kit-java/src/main/java/se/kth/ict/id2203/Assignment3abExecutor.java
@@ -8,73 +8,141 @@ import se.sics.kompics.launch.Topology;
  * @author ALEX & fingolfin
  */
 public class Assignment3abExecutor {
-   /**
+
+    /**
      * The main method.
      *
      * @param args the arguments
      */
     public static final void main(String[] args) {
 
-       Topology topologyEx1 = new Topology() {
+        Topology topologyEx12 = new Topology() {
             {
                 node(1, "127.0.0.1", 22030);
                 node(2, "127.0.0.1", 22031);
                 node(3, "127.0.0.1", 22032);
-                
+
                 defaultLinks(1000, 0);
             }
         };
-       
-       Topology topologyEx2 = new Topology() {};
-        
-        
-        Scenario scenario3a = new Scenario(Assignment3aMain.class){
+
+        Topology topologyEx3 = new Topology() {
+            {
+                node(1, "127.0.0.1", 22031);
+                node(2, "127.0.0.1", 22032);
+                node(3, "127.0.0.1", 22033);
+                link(1, 2, 1000, 0).bidirectional();
+                link(1, 3, 2000, 0).bidirectional();
+                link(2, 3, 1750, 0).bidirectional();
+            }
+        };
+
+        Topology topologyEx4 = new Topology() {
+            {
+                node(1, "127.0.0.1", 22031);
+                node(2, "127.0.0.1", 22032);
+                node(3, "127.0.0.1", 22033);
+                link(1, 2, 1000, 0).bidirectional();
+                link(1, 3, 2000, 0).bidirectional();
+                link(2, 3, 1750, 0).bidirectional();
+            }
+        };
+
+        Scenario scenarioEx1a = new Scenario(Assignment3aMain.class) {
             {
                 command(1, "D30000");
-                command(2, "D500:W4:D25000", 100);
-                command(3, "D10000:R", 200);
+                command(2, "D500:W4:D25000");
+                command(3, "D10000:R");
             }
         };
 
-        Scenario scenario3b = new Scenario(Assignment3bMain.class){
+        Scenario scenarioEx1b = new Scenario(Assignment3bMain.class) {
             {
                 command(1, "D30000");
-                command(2, "D500:W4:D25000", 100);
-                command(3, "D10000:R", 200);
+                command(2, "D500:W4:D25000");
+                command(3, "D10000:R");
             }
         };
-        Scenario scenario1 = new Scenario(Assignment2Main.class) {
+
+        Scenario scenarioEx2 = new Scenario(Assignment3bMain.class) {
             {
-                for (int i = 1; i <= 10; i++) {
-                    command(i, "Bmsgfrom"+i+":S1500:Bother_msg_from"+i);
-                    
-                }
-                
+                command(1, "D500:W5:R:D5000:R:D30000");
+                command(2, "D500:W6:R:D5000:R:D30000");
+                command(3, "D500:R:D500:R:D10000", 20000);
             }
         };
 
-        Scenario scenario2 = new Scenario(Assignment1aMain.class) {
+
+
+
+        Scenario scenario123 = new Scenario(Assignment3bMain.class) {
             {
-                command(1, "S2500:La1:S300:PA1:X").recover("S400:Pff", 1000);
-                command(2, "S2500:Pb2:S300:LB2");
-                command(3, "S2500:Lc3:S300:PC3");
-                command(4, "S2500:Pd4:S300:LD4");
+                command(1, "D500:W1:R:D500:R:D800");
+                command(2, "D500:W2:R:D500:R:D800", 100);
+                command(3, "D500:W3:R:D500:R:D800", 200);
             }
         };
 
-        
 
-        //scenarioPfd.executeOn(topology2);
-        //scenario1.executeOn(topology2);
-        scenario3a.executeOn(topologyEx1);
-        //scenario3b.executeOn(topologyEx1);
-        //scenario1.executeOn(topology2);
-        // scenario2.executeOn(topology1);
-        // scenario2.executeOn(topology2);
-        // scenario1.executeOnFullyConnected(topology1);
-        // scenario1.executeOnFullyConnected(topology2);
-        // scenario2.executeOnFullyConnected(topology1);
-        // scenario2.executeOnFullyConnected(topology2);
+        Scenario scenario132 = new Scenario(Assignment3bMain.class) {
+            {
+                command(1, "D500:W1:R:D500:R:D800");
+                command(3, "D500:W3:R:D500:R:D800", 100);
+                command(2, "D500:W2:R:D500:R:D800", 200);
+            }
+        };
+
+
+        Scenario scenario213 = new Scenario(Assignment3bMain.class) {
+            {
+                command(2, "D500:W2:R:D500:R:D800");
+                command(1, "D500:W1:R:D500:R:D800", 100);
+                command(3, "D500:W3:R:D500:R:D800", 200);
+            }
+        };
+
+
+        Scenario scenario231 = new Scenario(Assignment3bMain.class) {
+            {
+                command(2, "D500:W2:R:D500:R:D800");
+                command(3, "D500:W3:R:D500:R:D800", 100);
+                command(1, "D500:W1:R:D500:R:D800", 200);
+            }
+        };
+
+        Scenario scenario312 = new Scenario(Assignment3bMain.class) {
+            {
+                command(3, "D500:W3:R:D500:R:D800");
+                command(1, "D500:W1:R:D500:R:D800", 100);
+                command(2, "D500:W2:R:D500:R:D800", 200);
+            }
+        };
+
+        Scenario scenario321 = new Scenario(Assignment3bMain.class) {
+            {
+                command(3, "D500:W3:R:D500:R:D800");
+                command(2, "D500:W2:R:D500:R:D800", 100);
+                command(1, "D500:W1:R:D500:R:D800", 200);
+            }
+        };
+
+            scenarioEx1a.executeOn(topologyEx12);
+            //scenarioEx1b.executeOn(topologyEx12);
+            //scenarioEx2.executeOn(topologyEx12);
+            
+            //scenario123.executeOn(topologyEx3);
+            //scenario132.executeOn(topologyEx3);
+            //scenario213.executeOn(topologyEx3);
+            //scenario231.executeOn(topologyEx3);
+            //scenario312.executeOn(topologyEx3);
+            //scenario321.executeOn(topologyEx3);
+            
+            //scenario123.executeOn(topologyEx4);
+            //scenario132.executeOn(topologyEx4);
+            //scenario213.executeOn(topologyEx4);
+            //scenario231.executeOn(topologyEx4);
+            //scenario312.executeOn(topologyEx4);
+            //scenario321.executeOn(topologyEx4);
 
         System.exit(0);
         // move one of the below scenario executions above the exit for

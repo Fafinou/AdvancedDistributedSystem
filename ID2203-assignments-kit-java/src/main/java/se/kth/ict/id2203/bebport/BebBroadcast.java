46ace0c6b8cab02e8fe3c8bf44dfa2f8cd8756fe
alex_geo <a.georgoudaki@gmail.com>
2013-02-20 14:57:19 +0100

    resolve conflct

--- ID2203-assignments-kit-java/src/main/java/se/kth/ict/id2203/bebport/BebBroadcast.java
+++ ID2203-assignments-kit-java/src/main/java/se/kth/ict/id2203/bebport/BebBroadcast.java
@@ -11,33 +11,17 @@ import se.sics.kompics.Event;
  */
 public class BebBroadcast extends Event{
 
-<<<<<<< HEAD
-    private WriteMessage msg;
-    private ReadMessage remsg;
-=======
+
     private BebDeliver msg;
 
->>>>>>> branch 'master' of https://github.com/Fafinou/AdvancedDistributedSystem.git
 
-<<<<<<< HEAD
-    public BebBroadcast(WriteMessage writeMessage) {
-        msg = writeMessage;
-    }
 
-    public BebBroadcast(ReadMessage readMessage) {
-        remsg = readMessage;
-=======
     public BebBroadcast(BebDeliver msg) {
         this.msg = msg;
->>>>>>> branch 'master' of https://github.com/Fafinou/AdvancedDistributedSystem.git
     }
 
     public BebDeliver getMsg() {
         return msg;
     }
-    
-        public ReadMessage getRemsg() {
-        return remsg;
-    }
-    
+       
 }
